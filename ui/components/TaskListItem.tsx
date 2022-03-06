b05d7e00394fc28da365bf89bc97e4593e91b92f
nmanumr <nmanumr@gmail.com>
2022-03-06 11:31:08 +0500

    finalize deployemnt

---
+++ ui/components/TaskListItem.tsx
@@ -0,0 +1,47 @@
+import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
+import Axios from 'axios';
+import { mutate } from 'swr';
+import React from 'react';
+import { LocalUserTask, Priority, RemoteUserTask } from '../@types/task';
+
+export default function TaskListItem({ task, onEdit }: { task: RemoteUserTask, onEdit: (task: LocalUserTask) => any }) {
+  return (
+    <div key={task.publicId} className="rounded-md bg-white shadow flex items-center py-3 px-6 space-x-4">
+      <div className="grow">
+        <div className="w-full overflow-hidden text-ellipses">{task.title}</div>
+        <div className="flex flex-wrap text-sm">
+          {task.labels.map((l) => (
+            <div
+              key={l.publicId}
+              className="px-2.5 py-0.5 rounded-md text-xs font-medium bg-gray-200 text-gray-800 mt-2 mr-2 shrink-0"
+            >
+              {l.title}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="text-sm mr-6">
+        {/* @ts-ignore */}
+        {Priority[task.priority]}
+      </div>
+      <button
+        type="button" className="p-1"
+        onClick={() => {
+          const labels = task.labels.map((l) => l.title);
+          onEdit({ ...task, labels });
+        }}
+      >
+        <PencilIcon className="w-5 h-5 text-green-700" />
+      </button>
+      <button
+        type="button" className="p-1"
+        onClick={() => {
+          Axios.delete(`/api/todo/${task.publicId}`)
+            .then(() => mutate('/api/todo'));
+        }}
+      >
+        <TrashIcon className="w-5 h-5 text-red-700" />
+      </button>
+    </div>
+  );
+}
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,61 +1,23 @@
-import useSWR, { mutate } from 'swr';
-import Axios from 'axios';
+import useSWR from 'swr';
 import React, { useState } from 'react';
-import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/router';
 
-import {
-  LocalUserTask, Priority, RemoteResponse, RemoteUserTask,
-} from '../@types/task';
 import Button from '../components/Button';
-import EditCreateTaskModal from '../components/EditCreateTaskModal';
+import { isAuthenticated } from '../providers/auth';
+import TaskListItem from '../components/TaskListItem';
 import DashboardLayout from '../layouts/DashboardLayout';
-
-function TaskListItem({ task, onEdit }: { task: RemoteUserTask, onEdit: (task: LocalUserTask) => any }) {
-  return (
-    <div key={task.publicId} className="rounded-md bg-white shadow flex items-center py-3 px-6 space-x-4">
-      <div className="grow">
-        <div className="w-full overflow-hidden text-ellipses">{task.title}</div>
-        <div className="flex flex-wrap text-sm">
-          {task.labels.map((l) => (
-            <div
-              key={l.publicId}
-              className="px-2.5 py-0.5 rounded-md text-xs font-medium bg-gray-200 text-gray-800 mt-2 mr-2 shrink-0"
-            >
-              {l.title}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="text-sm mr-6">
-        {/* @ts-ignore */}
-        {Priority[task.priority]}
-      </div>
-      <button
-        type="button" className="p-1"
-        onClick={() => {
-          const labels = task.labels.map((l) => l.title);
-          onEdit({ ...task, labels });
-        }}
-      >
-        <PencilIcon className="w-5 h-5 text-green-700" />
-      </button>
-      <button
-        type="button" className="p-1"
-        onClick={() => {
-          Axios.delete(`/api/todo/${task.publicId}`)
-            .then(() => mutate('/api/todo'));
-        }}
-      >
-        <TrashIcon className="w-5 h-5 text-red-700" />
-      </button>
-    </div>
-  );
-}
+import EditCreateTaskModal from '../components/EditCreateTaskModal';
+import { LocalUserTask, RemoteResponse, RemoteUserTask } from '../@types/task';
 
 export default function HomePage() {
   const [isAddModalOpen, setAddModalOpen] = useState(false);
   const [selectedTask, setSelected] = useState<LocalUserTask>();
   const { data } = useSWR<RemoteResponse<RemoteUserTask>>('/api/todo');
+  const router = useRouter();
+
+  if (!isAuthenticated()) {
+    router.replace('/accounts/login');
+  }
 
   return (
     <DashboardLayout>
